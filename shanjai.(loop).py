76a0c2f0268abc5bd992a192c89b473e5f270648
shanjai839 <shanjaikannan184@gmail.com>
2026-05-05 11:12:46 +0530

    Add files via upload

---
+++ shanjai.(loop).py
@@ -0,0 +1,138 @@
+'''#1
+n=int(input("Enter number:"))
+for i in range(1,n+1,1):
+    print(i)
+'''
+'''#2
+n=int(input("Enter number:"))
+for i in range(2,n*2+1,2):
+    print(i)
+'''
+'''#3
+n=int(input("Enter number:"))
+for i in range(1,n*2+1,2):
+    print(i)
+'''
+'''#4
+n=int(input("Enter number:"))
+for i in range(3,n*3+1,3):
+    print(i)
+'''
+'''#5
+n=int(input("Enter number :"))
+for i in range(5,n*5+1,5):
+    print(i)
+'''
+'''#6
+n=int(input("Enter number :"))
+for i in range(2,n*1,2):
+    print(i)
+'''
+'''#7
+n=int(input("Enter number:"))
+for i in range(2,n+1,1):
+    if(i%2==0 or i%3==0):
+        print(i)
+'''
+'''#8
+n=int(input("Enter number:"))
+for i in range(2,n+1,1):
+    if (i%2==0 or i%5==0 or i%7==0):
+        print(i)
+'''
+'''#9
+n=int(input("Enter number:"))
+for i in range(3,n*2,1):
+    if (i%3==0 or i%5==0 or i%7==0):
+        print(i)
+'''
+'''#10
+n=int(input("Enter number:"))
+s=0
+while n>0:
+    r=n%10
+    s=s+r
+    n=n//10
+    print("Sun of number:",s)
+'''
+'''#11
+n=int(input("Enter number:"))
+c=0
+while n>0:
+    c=c+1
+    n=n//10
+    print("Count:",c)
+'''
+'''#12
+n=int(input("Enter number:"))
+for i in range(1,n+1):
+    if n%i==0:
+        print(i)
+'''
+'''#13
+
+
+n=int(input("enter number:"))
+count=0
+for i in range(1,n+1):
+    if n%i==0:
+        count+=1
+    print("number:",count)
+'''
+
+  
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
